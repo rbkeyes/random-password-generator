be4b6421a6604f54756d03de24c1f01b8a9a6442
Reed Keyes <rbkeyes@gmail.com>
2022-11-08 10:51:24 -0800

    pseudocode in test-script.js

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var password = generatePassword();
+  function generatePassword(length) {
+    var password           = " ";
+    // var characters       = "ABCDEFGHIJKLM?NOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789!#$%&*+-=?@_~";
+    var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    var num = "01234567890123456789";
+    var special = "!#$%&*+-=?@_~";
+    var characters = (alpha.concat(num + special));
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-function makeid(length) {
-    var result           = " ";
-    var characters       = "ABCDEFGHIJKLM?NOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789!#$%&*+-=?@_~";
-    // var characters = {
-    //   alpha: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-    //   num: "01234567890123456789",
-    //   special: "!#$%&*+-=?@_~",
-  
-    // console.log(characters),
-    // console.log(characters.alpha.length);
-    // console.log(characters.num.length);
-    // console.log(characters.special.length);
-  
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;
+    return result (password);
   }
+
+
+    var passwordtext = document.querySelector("#password");
+
+    passwordText.value = password;
   
-  console.log(makeid(8));
+  }
+
+
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+  
+//     // console.log(characters),
+//     // console.log(characters.alpha.length);
+//     // console.log(characters.num.length);
+//     // console.log(characters.special.length);
+  
+
+  
+//   console.log(makeid(8));
 
 // WHEN I click the button to generate a password 
 // THEN I am presented with a series of prompts for password criteria
---
+++ test-script.js
@@ -0,0 +1,9 @@
+// create variables for:
+// characters to be used (upper case, lower case, numerical, special).
+
+// prompt user to select desired variable types for password
+// user enter desired # of characters (>=8, <=128)
+
+// if user selection does not meet parameters, alert.
+
+// 
