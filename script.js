508ead94bd2c5aba532699bf90f49e98a1d7c918
Reed Keyes <rbkeyes@gmail.com>
2022-11-10 22:42:02 -0800

    added algorithm to set variables for password generator

--- script.js
+++ script.js
@@ -1,65 +1,83 @@
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// // // "generate" button
+var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var length = " "
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "01234567890123456789";
+var special = "!#$%&*+-=?@_~";
+var chars = []
+
+
+// // // Write password to the #password input
+generateBtn.addEventListener("click", writePassword);
 function writePassword() {
-//   var password = generatePassword();
-  function generatePassword(length) {
-    var password           = " ";
-    // var characters       = "ABCDEFGHIJKLM?NOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789!#$%&*+-=?@_~";
-    var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var num = "01234567890123456789";
-    var special = "!#$%&*+-=?@_~";
-    var characters = (alpha.concat(num + special));
 
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result (password);
+  // how many characters?
+  var setLength = window.prompt("How many characters should your password contain? Please enter a value between 8 and 128.");
+  if (!setLength) {
+    return;
+  } else if (setLength >= 8 && setLength <= 128) {
+    length = setLength;
+  } else if (setLength < 8 || setLength > 128) {
+    window.alert("Please try again. Number must be no less than 8 and no greater than 128.");
+    writePassword();
+  } else if (setLength != num) {
+    window.alert("Please enter a number.");
+    writePassword();
   }
 
-
-    var passwordtext = document.querySelector("#password");
-
-    passwordText.value = password;
-  
+  // include capital letters?
+  var setCase = window.confirm("Your password will be " + length + " characters long.\nWould you like to include capital letters?");
+  if (setCase) {
+    setCase = "Yes"
+    var setNum = window.confirm("Your password will include capital letters.\nShould we add some numbers?");
+    chars.push(upCase);
+  } else {
+    setCase = "No"
+    upCase = " ";
+    setNum = window.confirm("Got it, capital letters will be left out.\nShould we add some numbers?");
   }
 
+  // include numbers?
+  if (setNum) {
+    setNum = "Yes"
+    chars.push(num);
+    var setSpecial = window.confirm("Numbers will now be included.\nHow about some special characters?")
+  } else {
+    setNum = "No"
+    setSpecial = window.confirm("Nix the numbers, no problem.\nHow about some special characters?")
+    num = " ";
+  }
 
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
+  // include special characters?
+  if (setSpecial) {
+    window.alert("Special characters sound great!\nPress OK to review your choices.");
+    setSpecial = "Yes"
+    chars.push(special)
+    return;
+  } else {
+    window.alert("No special characters, no problem!\nPress OK to review your choices.");
+    setSpecial = "No"
+    special = " "
+    return;
+  }
   
-//     // console.log(characters),
-//     // console.log(characters.alpha.length);
-//     // console.log(characters.num.length);
-//     // console.log(characters.special.length);
-  
+}
 
-  
-//   console.log(makeid(8));
+var confirmChoices = window.confirm("We're all set! You chose:\nLength: " + length + " characters.\nLower case letters: Yes(default)\nUpper case letters: " + setCase + "\nNumbers: " + setNum + "\nSpecial characters: " + setSpecial + "\nPress OK to generate your new password! To start over, press cancel.");
 
-// WHEN I click the button to generate a password 
-// THEN I am presented with a series of prompts for password criteria
+var confirmChoices = window.confirm("We're all set! You chose:\nLength: " + length + " characters.\nLower case letters: Yes(default)\nUpper case letters: " + setCase + "\nNumbers: " + setNum + "\nSpecial characters: " + setSpecial + "\nPress OK to generate your new password! To start over, press cancel.");
+// // // Add event listener to generate button
+// // 
 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
+// writePassword()
 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
+// var password=document.getElementById("password");
 
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// var password = generatePassword();
+// var passwordText = document.querySelector("#password");
+// // generate password to screen
+// function genPassword() {
 
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+// }
