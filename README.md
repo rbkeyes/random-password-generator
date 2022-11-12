f05da43926eb292972b88326f4673a5b5fd44bfb
Reed Keyes <rbkeyes@gmail.com>
2022-11-11 23:22:15 -0800

    got it mostly working, just needs some fine-tuning

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ https://stackoverflow.com/questions/1349404/generate-random-string-characters-in
 <!-- run function with button click -->
 https://www.w3schools.com/jsref/met_win_confirm.asp
 
-<!-- simple -->
-https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm
 
 <!-- display text dependent on action -->
 https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm3
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,96 +3,90 @@ var generateBtn = document.querySelector("#generate");
 var password = document.getElementById("password");
 var passwordText = document.querySelector("#password");
 
-var length = ""
+var length = "";
+var characters = "";
+var genPassword = "";
 var lowCase = "abcdefghijklmnopqrstuvwxyz";
 var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "01234567890123456789";
 var special = "!#$%&*+-=?@_~";
-var characters = [lowCase]
-var password = ""
 
-// function to generate random password from length and chosen characters)
-function randomPassword() {
-  for (var i = 0; i < length.length; i++) {
-  password = characters.charAt(Math.floor(Math.random() * characters.length));
+
+function passRandom() {
+  for ( var i = 0; i < length; i++ ) {
+    genPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+    passwordText.textContent = genPassword;
   }
-  return password;
 }
-console.log(password);
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-  // create password
-  function writePassword() {
-    console.log(characters)
-
-    // how many characters?
-    var setLength = window.prompt("How many characters should your password contain? Please enter a value between 8 and 128.");
-    if (!setLength) {
-      return;
-    } else if (setLength >= 8 && setLength <= 128) {
-      length = setLength;
-    } else if (setLength < 8 || setLength > 128) {
-      window.alert("Please try again. Number must be no less than 8 and no greater than 128.");
-      writePassword();
+function userSelect() {
+// how many characters?
+var setLength = window.prompt("How many characters should your password contain? Please enter a value between 8 and 128.");
+if (!setLength) {
+    return;
+  } else if (setLength >= 8 && setLength <= 128) {
+    length = setLength;
+    characters += lowCase;
+  } else if (setLength < 8 || setLength > 128) {
+    window.alert("Please try again. Number must be no less than 8 and no greater than 128.");
+    userSelect();
     } else if (setLength != num) {
       window.alert("Please enter a number.");
-      writePassword();
+      userSelect();
     }
-
+    
     // include capital letters?
     var setCase = window.confirm("Your password will be " + length + " characters long.\nWould you like to include capital letters?");
     if (setCase) {
       setCase = "Yes"
       var setNum = window.confirm("Your password will include capital letters.\nShould we add some numbers?");
-      characters.push(upCase);
+      characters += upCase;
     } else {
       setCase = "No"
       upCase = " ";
       setNum = window.confirm("Got it, capital letters will be left out.\nShould we add some numbers?");
     }
-
+    
     // include numbers?
     if (setNum) {
       setNum = "Yes"
-      characters.push(num);
+      characters += num;
       var setSpecial = window.confirm("Numbers will now be included.\nHow about some special characters?")
     } else {
       setNum = "No"
       setSpecial = window.confirm("Nix the numbers, no problem.\nHow about some special characters?")
       num = " ";
     }
-
+    
     // include special characters?
     if (setSpecial) {
       window.alert("Special characters sound great!\nPress OK to review your choices.");
       setSpecial = "Yes"
-      characters.push(special)
+      characters += special;
     } else {
       window.alert("No special characters, no problem!\nPress OK to review your choices.");
       setSpecial = "No"
       special = " "
     }
-
+    
     // confirm selections
     var selection = window.confirm("We're all set! You chose:\nLength: " + length + " characters.\nLower case letters: Yes (default)\nUpper case letters: " + setCase + "\nNumbers: " + setNum + "\nSpecial characters: " + setSpecial + "\nPress OK to generate your new password! To start over, press cancel.");
     if (selection) {
-      password = randomPassword();
+      passRandom()
     } else {
       return;
     }
   }
+  
+
+function writePassword() {
+  
+} 
+ 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
 
-// writePassword()
 
-  // display password on screen
-
-
-// // generate password to screen
-// function genPassword() {
-
-// }
